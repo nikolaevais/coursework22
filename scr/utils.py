b2e68e3403dcf331d2c46f568f828a6057cba9d8
ASUS_TUF_FEO <nikolaeva_i_s@mail.ru>
2024-04-22 07:32:26 +0300

    in the utils file I wrote the function sorting_information_by_parameter_state

--- scr/utils.py
+++ scr/utils.py
@@ -9,3 +9,15 @@ def upload_file(filepath):
     operations = json.load(file)
     return operations
 
+
+def sorting_information_by_parameter_state(file):
+    '''
+    Получаем список словарей,
+    в которых state = EXECUTED
+    '''
+    operations = file
+    list_state_executed = []
+    for operation in operations:
+        if operation.get('state') == "EXECUTED":
+            list_state_executed.append(operation)
+    return list_state_executed
