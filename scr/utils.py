ccb6a677e2ea6ea81340ec7e3273288f4d0aaffa
ASUS_TUF_FEO <nikolaeva_i_s@mail.ru>
2024-04-22 07:32:53 +0300

    in the utils file I wrote the function sort_by_transaction_date

--- scr/utils.py
+++ scr/utils.py
@@ -21,3 +21,14 @@ def sorting_information_by_parameter_state(file):
         if operation.get('state') == "EXECUTED":
             list_state_executed.append(operation)
     return list_state_executed
+
+
+def sort_by_transaction_date(file, n):
+    '''
+    сортировка по дате операции
+    и сохранение пяти последних операции
+    '''
+    list_state_executed = file
+    list_date_sort = sorted(list_state_executed, key=lambda x: x['date'], reverse=True)
+    list_recent_transactions = list_date_sort[0:n]
+    return list_recent_transactions
